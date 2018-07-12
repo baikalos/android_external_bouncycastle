bda0c9dc1a07501b261384eafed29040482a5d7f
Neil Fuller <nfuller@google.com>
2018-07-12 15:58:48 +0100

    Use generated source not jarjar
    This commit switches to generating source code for
    com.android.org.bouncycastle used on Android devices rather than relying
    on jajar to repackage the bytecode.
    
    Source code can (and must) be regenerated using scripts in
    external/bouncycastle/srcgen when changes are made to bouncycastle
    source code.
    
    Having source code for the Android internal bouncycastle is being done
    to enable the code to be included in stubs for system and other internal
    API surfaces: the stubs generation relies on having source (which can be
    annotated, or just have associated API metadata).
    
    Test: build
    Test: CtsLibcoreTestCases
    Change-Id: I6d21774088e73c936e99059a9a0f071080174015

---
+++ srcgen/generate_android_src.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CLASSPATH=${ANDROID_HOST_OUT}/framework/currysrc.jar:${ANDROID_HOST_OUT}/framework/android_bouncycastle_srcgen.jar
+
+# Temporary hack: b/111389216
+zip -d ${ANDROID_HOST_OUT}/framework/currysrc.jar "META-INF/*"
+
+BOUNCY_CASTLE_DIR=${ANDROID_BUILD_TOP}/external/bouncycastle
+
+cd ${ANDROID_BUILD_TOP}
+make -j15 android_bouncycastle_srcgen
+
+function do_transform() {
+  SRC_IN_DIR=$1
+  SRC_OUT_DIR=$2
+
+  if [ ! -d $SRC_OUT_DIR ]; then
+    echo ${SRC_OUT_DIR} does not exist >&2
+    exit 1
+  fi
+  rm -rf ${SRC_OUT_DIR}
+  mkdir -p ${SRC_OUT_DIR}
+
+  java -cp ${CLASSPATH} com.android.bouncycastle.srcgen.BouncyCastleTransform ${SRC_IN_DIR} ${SRC_OUT_DIR}
+}
+
+BCPROV_SRC_IN_DIR=${BOUNCY_CASTLE_DIR}/bcprov/src/main/java
+BCPROV_SRC_OUT_DIR=${BOUNCY_CASTLE_DIR}/android_bcprov/src/main/java
+do_transform ${BCPROV_SRC_IN_DIR} ${BCPROV_SRC_OUT_DIR}
+
+BCPKIX_SRC_IN_DIR=${BOUNCY_CASTLE_DIR}/bcpkix/src/main/java
+BCPKIX_SRC_OUT_DIR=${BOUNCY_CASTLE_DIR}/android_bcpkix/src/main/java
+do_transform ${BCPKIX_SRC_IN_DIR} ${BCPKIX_SRC_OUT_DIR}
+
